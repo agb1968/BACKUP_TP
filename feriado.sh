8d12a56d5c952e29f2af5491e012a2d614bfa03f
Alejandro García Borboroglu <77503218+agb1968@users.noreply.github.com>
2023-11-08 08:42:43 -0300

    feriado.sh

---
+++ feriado.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Definir array asociativo para los feriados
+declare -A feriados
+feriados=(
+  ["0101"]="Año Nuevo"
+  ["2403"]="Día Nacional de la Memoria por la Verdad y la Justicia"
+  ["0101"]="Año Nuevo"
+  ["2002"]="Carnaval"
+  ["2102"]="Carnaval"
+  ["2403"]="Día Nacional de la Memoria por la Verdad y la Justicia"
+  ["0204"]="Viernes Santo"
+  ["0105"]="Día del Trabajador"
+  ["2505"]="Día de la Revolución de Mayo"
+  ["1706"]="Día del Paso a la Inmortalidad del General Martín Miguel de Güemes"
+  ["2006"]="Día del Paso a la Inmortalidad del General Manuel Belgrano"
+  ["0907"]="Día de la Independencia"
+  ["1508"]="Día de la Asunción de la Virgen"
+  ["1010"]="Día del Respeto a la Diversidad Cultural"
+  ["2011"]="Día de la Soberanía Nacional"
+  ["0812"]="Día de la Inmaculada Concepción de María"
+  ["2512"]="Navidad"
+  # Añadir más feriados aquí...
+)
+
+# Función para validar la fecha
+validarFecha() {
+  local fecha=$1
+  if ! [[ $fecha =~ ^[0-3][0-9][0-1][0-9][0-9]{4}$ ]]; then
+    echo "Formato de fecha incorrecto. Asegúrese de usar DDMMAAAA."
+    exit 1
+  fi
+}
+
+# Función para verificar si es fin de semana
+esFinDeSemana() {
+  local fecha=$1
+  local diaSemana
+
+  # Asumiendo GNU date
+  diaSemana=$(date -d "${fecha:4:4}-${fecha:2:2}-${fecha:0:2}" +%u)
+  
+  if [[ $diaSemana -eq 6 || $diaSemana -eq 7 ]]; then
+    echo "La fecha $fecha cae en fin de semana."
+    exit 0
+  fi
+}
+
+# Función para verificar si es feriado
+esFeriado() {
+  local fecha_larga=$1
+  local fecha_corta="${fecha_larga:0:4}"
+  if [[ ${feriados[$fecha_corta]+_} ]]; then
+    echo "La fecha $fecha_larga es un feriado: ${feriados[$fecha]}"
+    exit 0
+  else
+    echo "La fecha $fecha no es un feriado."
+  fi
+}
+
+# Verificar si se ha proporcionado un argumento
+if [[ $# -ne 1 ]]; then
+  echo "Uso: $0 DDMMAAAA"
+  exit 1
+fi
+
+fecha="$1"
+# Validar la fecha
+validarFecha "$fecha"
+# Verificar si es fin de semana
+esFinDeSemana "$fecha"
+# Verificar si es feriado
+esFeriado "$fecha"
